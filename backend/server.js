4500c63cf9a6930ec02718dc1edf8ef3137e9c65
ricoadtxx <euricoadit555@gmail.com>
2025-01-30 13:50:36 +0700

    deploy vercel

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors"; // ✅ Import cors
+import cors from "cors";
 import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
@@ -23,6 +23,8 @@ app.use(
 	cors({
 		origin: process.env.FRONTEND_URL,
 		credentials: true,
+		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+		allowedHeaders: ["Content-Type", "Authorization"],
 	})
 );
 
